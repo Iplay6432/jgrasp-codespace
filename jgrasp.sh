3f09bd80758263374886f73e8607a48996742187
Iplay6432 <71528024+Iplay6432@users.noreply.github.com>
2025-05-16 20:15:56 +0000

    testing auto install jgrasp

--- jgrasp.sh
+++ jgrasp.sh
@@ -5,8 +5,6 @@ wget https://www.jgrasp.org/dl4g/jgrasp/jgrasp206_17.zip
 
 unzip jgrasp206_17.zip
 
-cd Desktop
-echo "#!/bin/bash" > jgrasp.sh
-echo "cd ~/jgrasp"
-echo "java -jar ../jgrasp/jgrasp.jar" > jgrasp.sh
+cd ~/Desktop
+echo "#!/bin/bash \n cd ~/jgrasp \n java -jar ../jgrasp/jgrasp.jar" > jgrasp.sh
 chmod +x jgrasp.sh
